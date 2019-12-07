da7389d9d586366bc5592641ae62c9d33266aa29
Patrick Morley <56406494+PatrickMorley@users.noreply.github.com>
2019-12-07 12:09:14 -0500

    Uploaded User Creation Assignments

---
+++ CreateUser2.py
@@ -0,0 +1,43 @@
+#Patrick Morley
+#Create User
+
+import random
+import os
+import sys
+import crypt
+import re
+
+def createUserName(line):
+    x = line.find(" ")
+    line = line[0] + line[x:]
+    line = line.replace(' ', '')
+    line = line.replace('-', '')
+    line = line.replace('\'', '')
+    line = line.replace('\n', '')
+    line = line.lower()
+    return line
+
+def createPassword():
+    dictionary = ['bucket', 'volleyball', 'loaf', 'cycle', 'daffy', 'demonic', 'cobweb', 'carriage', 'sound', 'winter', 'button']
+    firstWord = random.choice(dictionary)
+    secondWord = random.choice(dictionary)
+    randnum = random.randint(1000, 9999)
+    password = (firstWord.lower() + str(randnum) + secondWord.upper())
+    return password
+
+with open("UserNamesLvl2", 'r') as namesfile:
+    for line in namesfile:
+        username = createUserName(line)
+        newpassword = createPassword()
+        with open("UsersLvl2", 'a') as usersfile:
+            usersfile.write("username: " + username + " password: " + newpassword + "\n")
+            encPass = crypt.crypt(newpassword, "22")
+            os.system("useradd p " + encPass + " " + username)
+            regex = (r"\b[a-z[a-z0-9]*\b")
+            if re.search(regex, username):
+                match = re.search(regex, username)
+                print("username valid")
+            else:
+                print("username invalid")
+                
+                    
